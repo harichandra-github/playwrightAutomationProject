a22591aa4531b310b53ca12ba1c5d18d4e806da9
harichandra-github <harichandrachaudhari21@gmail.com>
2025-05-21 02:59:51 +0530

    Added custom exceptions

---
+++ src/main/java/com/qa/saucedemo/exceptions/BrowserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.qa.saucedemo.exceptions;
+
+public class BrowserNotFoundException extends RuntimeException{
+	
+	public BrowserNotFoundException(String msg) {
+		super(msg);
+		
+	}
+	
+
+}
