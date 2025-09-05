7b7783f2a8893e5604d34f07f306875c674631d8
harichandra-github <harichandrachaudhari21@gmail.com>
2025-09-06 02:42:44 +0530

    updated the constants

--- src/main/java/com/qa/saucedemo/constants/AppConstants.java
+++ src/main/java/com/qa/saucedemo/constants/AppConstants.java
@@ -4,13 +4,21 @@ public class AppConstants {
     public static final String CONFIG_FILE_PATH="src/test/resources/config/config.properties";
 
 
-    final public static String NAME_ASCENDING="Name (A to Z)";
-    final public static String NAME_DESCENDING="Name (Z to A)";
-   final public static String PRICE_ASCENDING="Price (low to high)";
-    final public static String PRICE_DESCENDING="Price (high to low)";
+    final public static String TITLE = "Swag Labs";
+    final public static String URL = "https://www.saucedemo.com/inventory.html";
+    final public static String HOMEPAGE_URL = "https://www.saucedemo.com/inventory.html";
+    final public static String HOMEPAGE_TITLE = "Swag Labs";
+    public static final String LOGIN_ERROR_MESSAGE = "Epic sadface: Username and password do not match any user in this service";
+    public static final String LOCKED_OUT_USER_ERROR_MESSAGE = "Epic sadface: Sorry, this user has been locked out.";
+    public static final String USERNAME_REQUIRED_ERROR_MESSAGE = "Epic sadface: Username is required";
+    public static final String PASSWORD_REQUIRED_ERROR_MESSAGE = "Epic sadface: Password is required";
+    public static final String INVALID_CREDENTIALS_ERROR_MESSAGE = "Epic sadface: Username and password do not match any user in this service";
 
-    final public static String title="Swag Labs";
-    final public static String url="https://www.saucedemo.com/v1/inventory.html";
-    final public static String homePageUrl="https://www.saucedemo.com/v1/inventory.html";
-    final public static String homePageTitle="Swag Labs";
+
+    public static final class FilterOptions {
+        public static final String NAME_ASCENDING = "Name (A to Z)";
+        public static final String NAME_DESCENDING = "Name (Z to A)";
+        public static final String PRICE_ASCENDING = "Price (low to high)";
+        public static final String PRICE_DESCENDING = "Price (high to low)";
+    }
 }
